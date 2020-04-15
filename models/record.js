1a533c57656319f737e4422879c20da6b609c007
HUANG Zong-Rong <maibells0@yahoo.com.tw>
2020-04-15 18:10:17 +0800

    Modify config.json and create migration files

---
+++ models/record.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Record = sequelize.define('Record', {
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    date: DataTypes.STRING,
+    amount: DataTypes.NUMBER,
+    retailer: DataTypes.STRING
+  }, {});
+  Record.associate = function(models) {
+    // associations can be defined here
+  };
+  return Record;
+};
